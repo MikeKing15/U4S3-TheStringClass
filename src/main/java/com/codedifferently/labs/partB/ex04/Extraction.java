a7555ea7fade387919a012968bf1e9ac4bd98946
MikeKing15 <mikeking11152004@gmail.com>
2025-01-07 13:41:44 -0500

    Finished tests

--- src/main/java/com/codedifferently/labs/partB/ex04/Extraction.java
+++ src/main/java/com/codedifferently/labs/partB/ex04/Extraction.java
@@ -2,22 +2,19 @@ package com.codedifferently.labs.partB.ex04;
 
 public class Extraction {
 
-    public static String extraction(){
-        String response = "";
+    public static String extraction() {
         String greeting = "Learning Java is a good time!";
 
-        /* Your code goes here*/
         String part1 = greeting.substring(0, 5);
-        String part2 = greeting.substring(5, 10);
-        String part3 = greeting.substring(10);
+        String part2 = greeting.substring(7, 11);
+        String part3 = "g " + greeting.substring(9);      
 
-        response = part1 + "\n" + part2 + "\n" + part3;
+        String response = part1 + "\n" + part2 + "\n" + part3;
 
         return response;
-
     }
+
     public static void main(String[] args) {
-        String extractionOutput = extraction();
-        System.out.println(extractionOutput);
+        System.out.println(extraction());
     }
 }
